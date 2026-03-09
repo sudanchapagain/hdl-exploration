afef5d7eef012282ab7cd5622f7ee5742af519ab
Sudan Chapagain <101015050+sudanchapagain@users.noreply.github.com>
2026-03-09 16:19:10 +0545

    the remaining 2 braincells not functioning
    Signed-off-by: Sudan Chapagain <101015050+sudanchapagain@users.noreply.github.com>

--- flake.nix
+++ flake.nix
@@ -1,6 +1,6 @@
 {
   inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
-  outputs = { nixpkgs }: {
+  outputs = { self, nixpkgs }: {
     devShells.x86_64-linux.default =
       let pkgs = import nixpkgs { system = "x86_64-linux"; };
       in pkgs.mkShell {
